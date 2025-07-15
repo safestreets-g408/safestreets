bb29f9b52fe8972011f6d0355247c4717c7d6ef1
Abhinay2206 <bakkeraabhinay@gmail.com>
2025-07-15 16:11:48 +0530

    change server url

--- apps/admin-portal/src/config/constants.js
+++ apps/admin-portal/src/config/constants.js
@@ -2,7 +2,7 @@
 export const DRAWER_WIDTH = 270;
 
 // API
-export const API_BASE_URL = 'http://localhost:5030/api';
+export const API_BASE_URL = 'https://safestreets-backend.onrender.com/api';
 export const API_ENDPOINTS = {
   DAMAGE_REPORTS: '/damage',
   REPAIRS: '/repairs',
